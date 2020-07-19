2bfe0e4e662cf6af93bbdcf662fb95b4fec9ae8e
Nakamura <60052047+naakaamura@users.noreply.github.com>
2020-07-19 16:20:54 -0300

    We need stats otherwise mess happens

--- EtherealHorizons.cs
+++ EtherealHorizons.cs
@@ -23,35 +23,11 @@ namespace EtherealHorizons
         public override void Load()
         {
             instance = this;
-<<<<<<< HEAD
-=======
-        }
-
-        public override void MidUpdateTimeWorld()
-        {
->>>>>>> c22045657b96d74129264b856931c21e5edbbf96
-        }
+		}
 
         public override void Unload()
         {
             instance = null;
-        }
-
-        public override void PostSetupContent()
-        {
-            Mod bossChecklist = ModLoader.GetMod("BossChecklist");
-            if (bossChecklist != null)
-            {
-                bossChecklist.Call("AddBoss",
-                    0.9f,
-                    ModContent.NPCType<AwakeCheeks>(),
-                    (Func<bool>)(() => EtherealWorld.downedAwakeCheeks));
-                // Spawn Items
-                // Collection
-                // Loot
-                // Spawn Info (Optional)
-                // Despawn Message (Optional)
-            }
         }
     }
 }
