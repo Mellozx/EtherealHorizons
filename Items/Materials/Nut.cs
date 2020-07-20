5e5deaee82f04415b903ed59eb4024e5cdddcd79
Nakamura <60052047+naakaamura@users.noreply.github.com>
2020-07-19 23:27:27 -0300

    Added Nut

---
+++ Items/Materials/Nut.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EtherealHorizons.Items.Materials
+{
+    public class Nut : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nut");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 22;
+            item.height = 24;
+            item.rare = ItemRarityID.White;
+            item.value = Item.sellPrice(copper: 30);
+        }
+    }
+}
