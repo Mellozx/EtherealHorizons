18361dc99d8d158af93d83b3a1beec6f8d7c0c5b
Andrzinho15 <59664190+Andrzinho15@users.noreply.github.com>
2020-08-12 11:45:38 -0300

    And its projectiles

---
+++ Projectiles/Magic/PetalProj.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+
+namespace EtherealHorizons.Projectiles.Magic
+{
+	public class PetalProj : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Petal");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 10;
+			projectile.height = 16;
+			projectile.aiStyle = 2;
+			projectile.friendly = true;
+			projectile.penetrate = 5;
+			projectile.magic = true;
+			projectile.timeLeft = 100;
+			projectile.arrow = false;
+		}
+		public override void AI()
+		{
+			projectile.velocity = (projectile.velocity).RotatedBy(MathHelper.ToRadians(Main.rand.Next(90)));
+		}
+	}
+}
