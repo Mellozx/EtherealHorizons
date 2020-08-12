93bc378001151290b0ad23837b8fcd03cabec147
Andrzinho15 <59664190+Andrzinho15@users.noreply.github.com>
2020-08-11 23:09:50 -0300

    Added the Erg's Blade

---
+++ Items/Weapons/Melee/ErgBlade.cs
@@ -0,0 +1,52 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using EtherealHorizons.Items.Materials;
+using EtherealHorizons.Projectiles.Melee;
+using System.Collections.Generic;
+
+namespace EtherealHorizons.Items.Weapons.Melee
+{
+	public class ErgBlade : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+            DisplayName.SetDefault("Erg's Blade");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 14;
+			item.melee = true;
+			item.width = 48;
+			item.height = 52;
+			item.useTime = 23;
+			item.useAnimation = 23;
+			item.useStyle = 1;
+			item.knockBack = 4f;
+			item.value = Item.buyPrice(0, 0, 20, 12);
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.shoot = mod.ProjectileType("DustiliteShardProj");
+			item.shootSpeed = 10f;
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			if(Main.rand.Next(5) < 1) // a 1 in 5 chance
+			{
+				Projectile.NewProjectile(position.X, position.Y, speedX, speedY, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+		}
+        public override void AddRecipes()
+        {
+			var recipe = new ModRecipe(mod);
+			recipe.AddIngredient(mod.ItemType("DustiliteShard"), 12);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+        }
+	}
+}
