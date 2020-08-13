7696bcac7dd881f1c52fae71a14e0df575fc5686
Andrzinho15 <59664190+Andrzinho15@users.noreply.github.com>
2020-08-13 09:49:53 -0300

    Dustilite finally spawns, finally

--- EtherealWorld.cs
+++ EtherealWorld.cs
@@ -3,8 +3,12 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using Terraria.ModLoader.IO;
+using Terraria.World.Generation;
+using Terraria.GameContent.Generation;
+using EtherealHorizons.Tiles;
 
 namespace EtherealHorizons
 {
@@ -53,6 +57,29 @@ namespace EtherealHorizons
             BitsByte downed = reader.ReadByte();
 
             downedAwakeCheeks = downed[0];
+        }
+
+        public override void ModifyWorldGenTasks(List<GenPass> tasks, ref float totalWeight)
+        {
+            int shiniesIndex = tasks.FindIndex(ae => ae.Name.Equals("Shinies"));
+            if(shiniesIndex != - 1)
+            {
+                tasks.Insert(shiniesIndex + 1, new PassLegacy("Dustilite Ore", delegate (GenerationProgress generationProgress)
+                {
+                    generationProgress.Message = "Generating Dustilite";
+                    for(int i = 0;i<(int)(Main.maxTilesX * Main.maxTilesY * 0.005f);i++)
+                    {
+                        int x = WorldGen.genRand.Next(200,Main.maxTilesX - 200);
+                        int y = WorldGen.genRand.Next((int)WorldGen.rockLayerHigh,Main.maxTilesY - 300);
+                        Tile tile = Framing.GetTileSafely(x,y);
+                        if(tile.type == TileID.Sandstone || tile.type == TileID.Sand)
+                        {
+                            WorldGen.TileRunner(x,y,4,3,ModContent.TileType<DustiliteOre>());//Replace fossilOre with Dustilite Ore :wegud:
+                        }
+                    }
+                }
+                ));
+            }
         }
     }
 }
