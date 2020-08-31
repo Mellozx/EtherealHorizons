4e65ae37b1f134ed38a1093297b433798382b20c
Nakamura <60052047+naakaamura@users.noreply.github.com>
2020-08-31 12:33:04 -0300

    Rewrite starting right now

--- EtherealHorizons.cs
+++ EtherealHorizons.cs
@@ -1,44 +1,8 @@
-using Terraria;
-using Terraria.ID;
 using Terraria.ModLoader;
-using System.Collections.Generic;
-using Terraria.Localization;
 
 namespace EtherealHorizons
 {
-    public class EtherealHorizons : Mod
-    {
-        public static EtherealHorizons instance;
-		public static string PlaceholderTexture = "EtherealHorizons/PLACEHOLDER";
-		
-        public EtherealHorizons()
-        {
-            Properties = new ModProperties()
-            {
-                Autoload = true,
-                AutoloadGores = true,
-                AutoloadSounds = true,
-                AutoloadBackgrounds = true
-            };
-        }
-
-        public override void Load()
-        {
-            instance = this;
-		}
-
-        public override void Unload()
-        {
-            instance = null;
-        }
-        public override void AddRecipeGroups()
-        {
-            var group = new RecipeGroup(() => Language.GetTextValue("LegacyMisc.37") + "Gold Crown", new int[]
-            {
-                ItemID.GoldCrown,
-                ItemID.PlatinumCrown
-            });
-            RecipeGroup.RegisterGroup("EtherealHorizons:GoldCrown", group);
-        }
-    }
+	public class EtherealHorizons : Mod
+	{
+	}
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# EtherealHorizons
-Ethereal Horizons Mod for Terraria! Please, consider this is a WIP, you can check our progress here tho :p
