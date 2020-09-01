866d3bdf010244591afcbcaceb0584532a67f78c
Mellozx <67687232+Mellozx@users.noreply.github.com>
2020-09-01 15:38:17 +0300

    Ancient Twig Sets (Mage + Summoner)
    These use placeholder sprites for now
    Notes:
    Change Tooltips and Display names
    Modify Additional Bonuses

---
+++ Items/Armors/AncientTwigArmor/AncientTwigLeggings.cs
@@ -0,0 +1,39 @@
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+
+namespace EtherealHorizons.Items.Armors.AncientTwigArmor
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class AncientTwigLeggings : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Ancient Twig Leggings");
+			Tooltip.SetDefault("");
+
+		}
+
+		public override void SetDefaults() {
+			item.width = 18;
+			item.height = 18;
+			item.value = 10000; // 10 silver
+			item.rare = ItemRarityID.Green;
+			item.defense = 5;
+		}
+
+		/* public override void UpdateEquip(Player player) {
+			player.moveSpeed += 0.02f;
+		}
+		*/
+		//im unsure whether to add this or not so its up to you
+
+		    public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod, "AncientTwig",15);
+			recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+		}
+	}
+}
---
+++ Items/Armors/AncientTwigArmor/AncientTwigSummoner.cs
@@ -0,0 +1,44 @@
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+
+namespace EtherealHorizons.Items.Armors.AncientTwigArmor
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class AncientTwigSummoner : ModItem
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Ancient Twig Summoner");
+			Tooltip.SetDefault("Increased Summoner Damage"
+				+ "\nAdditional 1 Max Minion");
+		}
+
+		public override void SetDefaults() {
+			item.width = 18;
+			item.height = 18;
+			item.value = 10000; // 10 silver
+			item.rare = ItemRarityID.Green;
+			item.defense = 4;
+		}
+
+		public override bool IsArmorSet(Item head, Item body, Item legs) {
+			return body.type == ItemType<AncientTwigChestPlate>() && legs.type == ItemType<AncientTwigLeggings>();
+		}
+
+		public override void UpdateArmorSet(Player player) {
+			
+			player.minionDamage -= 0.1f; // Minions Deal More Damage
+			player.maxMinions += 1; // 1 More Max Minion 
+			
+		}
+
+		    public override void AddRecipes() {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(mod, "AncientTwig",12);
+			recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+		}
+	}
+}
