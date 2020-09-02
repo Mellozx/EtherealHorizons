d2832a016653b2f9b8267bab86e844f549636df4
Naka <60052047+naakaamura@users.noreply.github.com>
2020-09-02 18:52:46 -0300

    Yes

---
+++ NPCs/Bosses/Chronos/Chronos.cs
@@ -0,0 +1,17 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace EtherealHorizons.NPCs.Bosses.Chronos
+{
+    public class Chronos : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Chronos");
+        }
+    }
+}
