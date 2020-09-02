3ac96c1ead060b29c0535e3044a5ff1993214462
Naka <60052047+naakaamura@users.noreply.github.com>
2020-09-02 19:32:15 -0300

    Update Chronos.cs

--- NPCs/Bosses/Chronos/Chronos.cs
+++ NPCs/Bosses/Chronos/Chronos.cs
@@ -16,13 +16,36 @@ namespace EtherealHorizons.NPCs.Bosses.Chronos
             DisplayName.SetDefault("Chronos");
         }
 
+        public override void ScaleExpertStats(int numPlayers, float bossLifeScale)
+        {
+            npc.lifeMax = (int)npc.lifeMax * 0.6f * bossLifeScale;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            npc.spriteDirection = npc.direction;
+        }
+
+        public override void HitEffect(int hitDirection, double damage)
+        {
+            if (npc.life <= 0) 
+            {
+            }
+        }
+
         public override void BossLoot(ref string name, ref int potionType)
         {
+            potionType = ItemID.HealingPotion;
+
             if (Main.netMode == NetmodeID.Server)
             {
                 // EtherealWorld.downedChronos = true;
                 NetMessage.SendData(MessageID.WorldData);
             }
+        }
+
+        public override void NPCLoot()
+        {
         }
 
         public override void AI()
@@ -33,6 +56,12 @@ namespace EtherealHorizons.NPCs.Bosses.Chronos
         private void Target()
         {
             player = Main.player[npc.target];
+        }
+
+        public override bool DrawHealthBar (byte hbPosition, ref float scale, ref Vector2 position)
+        {
+            scale = 1.5f
+            return null;
         }
     }
 }
