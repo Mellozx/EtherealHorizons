0e9ecac20bd3b58cfd9b94f60b31904a41f110d1
Naka <60052047+naakaamura@users.noreply.github.com>
2020-09-03 18:21:27 -0300

    Update OldenHeadgear.cs

--- Items/Armor/Olden/OldenHeadgear.cs
+++ Items/Armor/Olden/OldenHeadgear.cs
@@ -1,44 +1,49 @@
 using Terraria;
-using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 using Terraria.ID;
+using Terraria.ModLoader;
+using EtherealHorizons.Items.Materials;
 
 namespace EtherealHorizons.Items.Armor.Olden
 {
 	[AutoloadEquip(EquipType.Head)]
 	public class OldenHeadgear : ModItem
 	{
-		public override void SetStaticDefaults() {
+		public override void SetStaticDefaults()
+                {
 			DisplayName.SetDefault("Olden Headgear");
 			Tooltip.SetDefault("Increased Summoner Damage"
 				+ "\nAdditional 1 Max Minion");
 		}
 
-		public override void SetDefaults() {
-			item.width = 18;
-			item.height = 18;
-			item.value = 10000; // 10 silver
-			item.rare = ItemRarityID.Green;
-			item.defense = 4;
+		public override void SetDefaults()
+                {
+			item.width = 20;
+			item.height = 20;
+			item.value = Item.sellPrice(silver: 10); // 10 silver
+			item.rare = ItemRarityID.Blue;
+			item.defense = 2;
 		}
 
-		public override bool IsArmorSet(Item head, Item body, Item legs) {
-			return body.type == ItemType<OldenMantle>() && legs.type == ItemType<OldenLeggings>();
+		public override bool IsArmorSet(Item head, Item body, Item legs) 
+                {
+			return body.type == ModContent.ItemType<OldenMantle>() && legs.type == ModContent.ItemType<OldenLeggings>();
 		}
 
-		public override void UpdateArmorSet(Player player) {
+		public override void UpdateArmorSet(Player player) 
+                {
 			
-			player.minionDamage -= 0.1f; // Minions Deal More Damage
+			player.minionDamage += 0.1f; // Minions Deal More Damage
 			player.maxMinions += 1; // 1 More Max Minion 
 			
 		}
 
-		    public override void AddRecipes() {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(mod, "AncientTwig",12);
-			recipe.AddTile(TileID.Anvils);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
+		public override void AddRecipes() 
+                {
+                    var recipe = new ModRecipe(mod);
+                    recipe.AddIngredient(ModContent.ItemType<AncientTwig>(), 3);
+		    recipe.AddTile(TileID.Anvils);
+                    recipe.SetResult(this);
+                    recipe.AddRecipe();
 		}
 	}
 }
