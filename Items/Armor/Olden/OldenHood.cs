110eae4baedd629eb24cb57290d555538cee980f
Naka <60052047+naakaamura@users.noreply.github.com>
2020-09-03 17:24:15 -0300

    Code check

--- Items/Armor/Olden/OldenHood.cs
+++ Items/Armor/Olden/OldenHood.cs
@@ -1,43 +1,47 @@
 using Terraria;
-using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace EtherealHorizons.Items.Armor.Olden
 {
 	[AutoloadEquip(EquipType.Head)]
 	public class OldenHood : ModItem
 	{
-		public override void SetStaticDefaults() {
+		public override void SetStaticDefaults()
+                {
 			DisplayName.SetDefault("Olden Hood");
 			Tooltip.SetDefault("Increased Magic Damage");
 		}
 
-		public override void SetDefaults() {
+		public override void SetDefaults() 
+                {
 			item.width = 18;
 			item.height = 18;
-			item.value = 10000; // 10 silver
-			item.rare = ItemRarityID.Green;
+			item.value = Item.sellPrice(silver: 40); // 10 silver
+			item.rare = ItemRarityID.Blue;
 			item.defense = 4;
 		}
 
-		public override bool IsArmorSet(Item head, Item body, Item legs) {
-			return body.type == ItemType<OldenMantle>() && legs.type == ItemType<OldenLeggings>();
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+                {
+			return body.type == ModContent.ItemType<OldenMantle>() && legs.type == ModContent.ItemType<OldenLeggings>();
 		}
 
-		public override void UpdateArmorSet(Player player) {
+		public override void UpdateArmorSet(Player player)
+                {
 			player.magicDamage -= 0.1f; // higher magic damage 
 			player.statManaMax2 += 1; // more mana kek, could be removed
 			
 			
 		}
 
-		    public override void AddRecipes() {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(mod, "AncientTwig",10);
-			recipe.AddTile(TileID.Anvils);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
+	        public override void AddRecipes() 
+                {
+                    var recipe = new ModRecipe(mod);
+                    recipe.AddIngredient(ModContent.ItemType<AncientTwig>(), 3);
+	            recipe.AddTile(TileID.WorkBenches);
+                    recipe.SetResult(this);
+                    recipe.AddRecipe();
 		}
 	}
 }
