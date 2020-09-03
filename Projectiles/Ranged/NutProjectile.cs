a4c03842b310c20e93295739ea7e0b8a33f22dbf
Naka <60052047+naakaamura@users.noreply.github.com>
2020-09-03 13:38:47 -0300

    Create NutProjectile.cs

---
+++ Projectiles/Ranged/NutProjectile.cs
@@ -0,0 +1,37 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace EtherealHorizons.Projectiles.Ranged
+{
+    public class NutProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Nut");
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.ranged = true;
+            projectile.ignoreWater = true;
+            projectile.tileCollide = true;
+            projectile.timeLeft = 180;
+            projectile.width = 20;
+            projectile.height = 20;
+            projectile.aiStyle = 0;
+        }
+
+        public override void AI()
+        {
+            projectile.rotation = 0.2f * projectile.direction;
+            projectile.velocity.X *= 0.90f;
+            projectile.velocity.Y += 0.10f;
+        }
+
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(SoundID.Dig, projectile.position);
+        }
+    }
+}
