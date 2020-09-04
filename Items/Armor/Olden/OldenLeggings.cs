e479e7668c8bc373a0973f9ce596ecdf6639a8ca
Naka <60052047+naakaamura@users.noreply.github.com>
2020-09-03 22:37:21 -0300

    Update OldenLeggings.cs

--- Items/Armor/Olden/OldenLeggings.cs
+++ Items/Armor/Olden/OldenLeggings.cs
@@ -1,39 +1,42 @@
 using Terraria;
 using Terraria.ModLoader;
-using static Terraria.ModLoader.ModContent;
 using Terraria.ID;
+using EtherealHorizons.Items.Materials;
 
 namespace EtherealHorizons.Items.Armor.Olden
 {
 	[AutoloadEquip(EquipType.Legs)]
 	public class OldenLeggings : ModItem
 	{
-		public override void SetStaticDefaults() {
+		public override void SetStaticDefaults()
+                {
 			DisplayName.SetDefault("Olden Leggings");
 			Tooltip.SetDefault("");
-
 		}
 
-		public override void SetDefaults() {
-			item.width = 18;
-			item.height = 18;
-			item.value = 10000; // 10 silver
-			item.rare = ItemRarityID.Green;
-			item.defense = 5;
+		public override void SetDefaults() 
+                {
+			item.width = 20;
+			item.height = 20;
+			item.value = Item.sellPrice(silver: 20); // 2858w8r884ilver
+			item.rare = ItemRarityID.Blue;
+			item.defense = 3;
 		}
 
-		/* public override void UpdateEquip(Player player) {
-			player.moveSpeed += 0.02f;
+		public override void UpdateEquip(Player player)
+                {
+			player.moveSpeed += 0.05f;
 		}
-		*/
+		
 		//im unsure whether to add this or not so its up to you
 
-		    public override void AddRecipes() {
-            ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(mod, "AncientTwig",15);
-			recipe.AddTile(TileID.Anvils);
-            recipe.SetResult(this);
-            recipe.AddRecipe();
+	        public override void AddRecipes()
+                {
+                    var recipe = new ModRecipe(mod);
+                    recipe.AddIngredient(ModContent.ItemType<WildlifeFragment>(), 4);
+		    recipe.AddTile(TileID.Anvils);
+                    recipe.SetResult(this);
+                    recipe.AddRecipe();
 		}
 	}
 }
