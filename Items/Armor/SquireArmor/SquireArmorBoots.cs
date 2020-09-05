a0212d16f387ea23c13af946ecb8cebbffa44b43
Lemmy-Koopa <56778754+Lemmy-Koopa@users.noreply.github.com>
2020-09-04 22:00:32 -0400

    Squire Moment

---
+++ Items/Armor/SquireArmor/SquireArmorBoots.cs
@@ -0,0 +1,31 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace EtherealHorizons.Items.Armors.SquireArmor
+{
+	[AutoloadEquip(EquipType.Legs)]
+	public class SquireArmorBoots : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Squire Boots");
+			Tooltip.SetDefault("5% increased max run speed.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 30;
+			item.height = 30;
+			item.defense = 3;
+			item.value = 25000;
+
+		}
+
+		public override void UpdateArmorSet(Player player)
+		{
+			player.maxRunSpeed += 0.5f;
+		}
+	}
+}
---
+++ Items/Armor/SquireArmor/SquireArmorHelmet.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using System;
+using static Terraria.ModLoader.ModContent;
+
+
+namespace EtherealHorizons.Items.Armors.SquireArmor
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class SquireArmorHelmet : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Squire Helmet");
+			Tooltip.SetDefault("3% increased movement speed");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 8;
+			item.height = 20;
+			item.defense = 1;
+			item.value = 2000;
+
+		}
+
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return body.type == ItemType<SquireArmorChestplate>() && legs.type == ItemType<SquireArmorBoots>();
+		}
+
+		public override void UpdateArmorSet(Player player)
+		{
+			player.moveSpeed += 0.3f;
+			
+		}
+	}
+}
