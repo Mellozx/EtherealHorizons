503792f48d2c5773ea7c8f314c4e9e085c17071b
Lemmy-Koopa <56778754+Lemmy-Koopa@users.noreply.github.com>
2020-09-04 22:16:40 -0400

    Thorn Moment

---
+++ Items/Materials/Thorn.cs
@@ -0,0 +1,20 @@
+﻿using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace EtherealHorizons.Items.Materials
+{
+
+	public class Thorn : ModItem 
+	{
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 22;
+            item.maxStack = 999;
+            item.rare = ItemRarityID.White;
+            item.value = (Item.sellPrice(0, 0, 0, 25));
+        }
+    }	
+}
