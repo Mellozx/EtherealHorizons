48e312e12465c2a2703e127c7e1422602cf69e92
Naka <60052047+naakaamura@users.noreply.github.com>
2020-09-05 15:07:37 -0300

    Update ThornAxe.cs

--- Items/Tools/ThornAxe.cs
+++ Items/Tools/ThornAxe.cs
@@ -8,14 +8,16 @@ namespace EtherealHorizons.Items.Tools
     {
         public override void SetDefaults()
         {
-            item.width = 34;
-            item.height = 42;
+            item.melee = true;
+            item.useTurn = true;
+            item.width = 20;
+            item.height = 20;
             item.damage = 5;
             item.useStyle = ItemUseStyleID.SwingThrow;
             item.useTime = 30;
             item.useAnimation = 30;
-            item.axe = 40;
-            item.knockBack = 6;
+            item.axe = 8;
+            item.knockBack = 5f;
             item.rare = ItemRarityID.White;
             item.value = Item.sellPrice(0, 0, 0, 80);
             item.autoReuse = false;
@@ -23,7 +25,7 @@ namespace EtherealHorizons.Items.Tools
 
         public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
         {
-            target.AddBuff(BuffID.Poisoned, 300);
+            target.AddBuff(BuffID.Poisoned, 180);
         }
     }
 }
