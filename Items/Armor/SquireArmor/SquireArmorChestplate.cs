9277542172dba3726f9eb6e381789c76fe8906b8
Mellozx <67687232+Mellozx@users.noreply.github.com>
2020-09-05 05:13:58 +0300

    Namespace fix 2/3

--- Items/Armor/SquireArmor/SquireArmorChestplate.cs
+++ Items/Armor/SquireArmor/SquireArmorChestplate.cs
@@ -3,7 +3,7 @@ using Terraria;
 using Terraria.ID;
 using static Terraria.ModLoader.ModContent;
 
-namespace EtherealHorizons.Items.Armors.SquireArmor
+namespace EtherealHorizons.Items.Armor.SquireArmor
 {
 	[AutoloadEquip(EquipType.Body)]
 	public class SquireArmorChestplate : ModItem
